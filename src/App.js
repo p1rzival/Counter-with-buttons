245baceacfee860f8efeadd8b25a8f3a76a81de6
Daniel-W <p1rzival@gmail.com>
2021-11-22 13:26:05 +0700

    I created a counter with couple the functions plus and minus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {nanoid} from "nanoid";
+
+
+const object = [
+  {id: 1, value: 1, buttons: [1]},
+  {id: 2, value: 2, buttons: [1, 2]},
+  {id: 3, value: 3, buttons: [1, 2, 3]}
+]
 
 function App() {
+
+  const [counter, setCounter] = useState(object)
+
+  const plus = (id, button) => {
+    const newCounter = counter.map(el => el.id === id ? {...el, value: el.value + button} : el)
+    setCounter(newCounter)
+  }
+
+  const minus = (id, button) => {
+    const newCounter = counter.map(el => el.id === id ? {...el, value: el.value - button} : el)
+    setCounter(newCounter)
+  }
+
+  const [nextValue, setNextValue] = useState(4)
+
+  const addCounter = () => {
+    const newCounter = [...counter, {id: nanoid(), value: nextValue, buttons: [1, 2, 3, 4]}]
+    setCounter(newCounter)
+    setNextValue(nextValue)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+
+        {counter.map(el => <div>
+          {el.buttons.map(button => <button onClick={() => plus(el.id, button)}>{+button}</button>)}
+          {el.value}
+          {el.buttons.map(button => <button onClick={() => minus(el.id, button)}>{-button}</button>)}
+
+
+        </div>)}
+
+        <button onClick={addCounter}>Add new Counter</button>
+
+      </div>
   );
 }
 
 export default App;
+
